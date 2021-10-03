0685c09fbd7ec02da288654c04165d22a4bc2280
Isuru Madusanka <isuru@st-tech.co>
2021-10-03 23:22:17 +0100

    Added Cronjob functionality

--- main.py
+++ main.py
@@ -2,28 +2,25 @@ import argparse
 import configparser
 import sys
 import yaml
+from datetime import datetime
 
 def add_numbers(number, other_number, output):
     result = number * other_number
-    print(f"The result is {result}", file=output)
+    print(f'[{datetime.utcnow().isoformat()}] The result is {result}', file=output)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n1', type=int, help='A number', default=1)
-    parser.add_argument('-n2', type=int, help='Another number', default=1)
-    # Added Configuration Parser
-    #parser.add_argument('--config', '-c', type=argparse.FileType('r'), help='Config File')
-    parser.add_argument('--config', '-c', type=argparse.FileType('r'), help='Config File in YAML')
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.rgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--config', '-c', type=argparse.FileType('r'), help='config file', default='/etc/automate.ini')
     # Added Argument Parsing Functionality
     parser.add_argument('-o', dest='output', type=argparse.FileType('w'), help='output file', default=sys.stdout)
-
 
     args = parser.parse_args()
 
     if args.config:
-        #config = configparser.ConfigParser()
-        #config.read_file(args.config)
-        config = yaml.load(args.config, Loader=yaml.FullLoader)
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
         # Transforming values into integers
         args.n1 = int(config['ARGUMENTS']['n1'])
         args.n2 = int(config['ARGUMENTS']['n2'])
